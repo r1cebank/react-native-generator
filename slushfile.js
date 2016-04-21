fb586fa0f61c3af6990cc1f36c87562439fa523b
Siyuan Gao <siyuangao@gmail.com>
2016-04-20 22:31:12 -0700

    Initial Commit

---
+++ slushfile.js
@@ -0,0 +1,91 @@
+/*
+ * slush-react-native-generator
+ * https://github.com/r1cebank/slush-react-native-generator
+ *
+ * Copyright (c) 2016, Siyuan Gao
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+    install = require('gulp-install'),
+    conflict = require('gulp-conflict'),
+    template = require('gulp-template'),
+    rename = require('gulp-rename'),
+    _ = require('underscore.string'),
+    inquirer = require('inquirer'),
+    path = require('path');
+
+function format(string) {
+    var username = string.toLowerCase();
+    return username.replace(/\s/g, '');
+}
+
+var defaults = (function () {
+    var workingDirName = path.basename(process.cwd()),
+      homeDir, osUserName, configFile, user;
+
+    if (process.platform === 'win32') {
+        homeDir = process.env.USERPROFILE;
+        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+    }
+    else {
+        homeDir = process.env.HOME || process.env.HOMEPATH;
+        osUserName = homeDir && homeDir.split('/').pop() || 'root';
+    }
+
+    configFile = path.join(homeDir, '.gitconfig');
+    user = {};
+
+    if (require('fs').existsSync(configFile)) {
+        user = require('iniparser').parseSync(configFile).user;
+    }
+
+    return {
+        appName: workingDirName,
+        userName: osUserName || format(user.name || ''),
+        authorName: user.name || '',
+        authorEmail: user.email || ''
+    };
+})();
+
+gulp.task('default', function (done) {
+    var prompts = [{
+        name: 'package',
+        message: 'What is the package name of your project?',
+        default: 'com.appname.app'
+    }, {
+        name: 'appName',
+        message: 'What is the name of your app'
+    }, {
+        name: 'appVersion',
+        message: 'What is the starting version of your app?',
+        default: '0.1.0'
+    }, {
+        type: 'confirm',
+        name: 'moveon',
+        message: 'Continue?'
+    }];
+    //Ask
+    inquirer.prompt(prompts,
+        function (answers) {
+            if (!answers.moveon) {
+                return done();
+            }
+            answers.appName = _.slugify(answers.appName);
+            gulp.src(__dirname + '/templates/**')
+                .pipe(template(answers))
+                .pipe(rename(function (file) {
+                    if (file.basename[0] === '_') {
+                        file.basename = '.' + file.basename.slice(1);
+                    }
+                }))
+                .pipe(conflict('./'))
+                .pipe(gulp.dest('./'))
+                .pipe(install())
+                .on('end', function () {
+                    done();
+                });
+        });
+});
---
+++ templates/index.android.js
@@ -0,0 +1,9 @@
+
+import React from "react-native";
+import Root from "./src/containers/root";
+
+const {
+  AppRegistry
+} = React;
+
+AppRegistry.registerComponent("react_native_boilerplate", () => Root);
